563d780606237ecf2b267cfc98e0f1fd8a3ca51a
ketencimert <mk4139@columbia.edu>
2025-06-02 19:12:47 -0400

    fix bash.

--- run_experiments.sh
+++ run_experiments.sh
@@ -9,17 +9,17 @@ Created on Mon Jun  2 15:43:53 2025
 
 # List of different flags for each run
 FLAGS_LIST=(
-"--d_hid 150 --d_emb 20 --n_layers 4 --dataset icu --device cuda:0 --phi_net masked --act elu --model_id 0 --device cuda:0 --norm None --early_stop 40 --cv_folds 5",
-"--d_hid 150 --d_emb 20 --n_layers 4 --dataset fico --device cuda:1 --phi_net masked --act elu --model_id 0 --device cuda:1 --norm None --early_stop 40 --cv_folds 5",
-"--d_hid 150 --d_emb 20 --n_layers 4 --dataset spambase --device cuda:2 --phi_net masked --act elu --model_id 0 --device cuda:2 --norm None --early_stop 40 --cv_folds 5",
-"--d_hid 150 --d_emb 20 --n_layers 4 --dataset adult --device cuda:3 --phi_net masked --act elu --model_id 0 --device cuda:3 --norm None --early_stop 40 --cv_folds 5",
-"--d_hid 150 --d_emb 20 --n_layers 4 --dataset icu --device cuda:0 --phi_net masked --act elu --model_id 0 --device cuda:0 --norm None --early_stop 40 --cv_folds 5 --norm layer",
-"--d_hid 150 --d_emb 20 --n_layers 4 --dataset fico --device cuda:1 --phi_net masked --act elu --model_id 0 --device cuda:1 --norm None --early_stop 40 --cv_folds 5 --norm layer",
-"--d_hid 150 --d_emb 20 --n_layers 4 --dataset spambase --device cuda:2 --phi_net masked --act elu --model_id 0 --device cuda:2 --norm None --early_stop 40 --cv_folds 5 --norm layer",
-"--d_hid 150 --d_emb 20 --n_layers 4 --dataset adult --device cuda:3 --phi_net masked --act elu --model_id 0 --device cuda:3 --norm None --early_stop 40 --cv_folds 5 --norm layer",
-"--d_hid 500 --d_emb 30 --n_layers 4 --dataset icu --device cuda:0 --phi_net masked --act elu --model_id 0 --device cuda:0 --norm None --early_stop 40 --cv_folds 5 --cont",
-"--d_hid 500 --d_emb 30 --n_layers 4 --dataset fico --device cuda:1 --phi_net masked --act elu --model_id 0 --device cuda:1 --norm None --early_stop 40 --cv_folds 5 --cont",
-"--d_hid 500 --d_emb 30 --n_layers 4 --dataset spambase --device cuda:2 --phi_net masked --act elu --model_id 0 --device cuda:2 --norm None --early_stop 40 --cv_folds 5 --cont",
+"--d_hid 150 --d_emb 20 --n_layers 4 --dataset icu --device cuda:0 --phi_net masked --act elu --model_id 0 --device cuda:0 --norm None --early_stop 40 --cv_folds 5"
+"--d_hid 150 --d_emb 20 --n_layers 4 --dataset fico --device cuda:1 --phi_net masked --act elu --model_id 0 --device cuda:1 --norm None --early_stop 40 --cv_folds 5"
+"--d_hid 150 --d_emb 20 --n_layers 4 --dataset spambase --device cuda:2 --phi_net masked --act elu --model_id 0 --device cuda:2 --norm None --early_stop 40 --cv_folds 5"
+"--d_hid 150 --d_emb 20 --n_layers 4 --dataset adult --device cuda:3 --phi_net masked --act elu --model_id 0 --device cuda:3 --norm None --early_stop 40 --cv_folds 5"
+"--d_hid 150 --d_emb 20 --n_layers 4 --dataset icu --device cuda:0 --phi_net masked --act elu --model_id 0 --device cuda:0 --norm None --early_stop 40 --cv_folds 5 --norm layer"
+"--d_hid 150 --d_emb 20 --n_layers 4 --dataset fico --device cuda:1 --phi_net masked --act elu --model_id 0 --device cuda:1 --norm None --early_stop 40 --cv_folds 5 --norm layer"
+"--d_hid 150 --d_emb 20 --n_layers 4 --dataset spambase --device cuda:2 --phi_net masked --act elu --model_id 0 --device cuda:2 --norm None --early_stop 40 --cv_folds 5 --norm layer"
+"--d_hid 150 --d_emb 20 --n_layers 4 --dataset adult --device cuda:3 --phi_net masked --act elu --model_id 0 --device cuda:3 --norm None --early_stop 40 --cv_folds 5 --norm layer"
+"--d_hid 500 --d_emb 30 --n_layers 4 --dataset icu --device cuda:0 --phi_net masked --act elu --model_id 0 --device cuda:0 --norm None --early_stop 40 --cv_folds 5 --cont"
+"--d_hid 500 --d_emb 30 --n_layers 4 --dataset fico --device cuda:1 --phi_net masked --act elu --model_id 0 --device cuda:1 --norm None --early_stop 40 --cv_folds 5 --cont"
+"--d_hid 500 --d_emb 30 --n_layers 4 --dataset spambase --device cuda:2 --phi_net masked --act elu --model_id 0 --device cuda:2 --norm None --early_stop 40 --cv_folds 5 --cont"
 "--d_hid 500 --d_emb 30 --n_layers 4 --dataset adult --device cuda:3 --phi_net masked --act elu --model_id 0 --device cuda:3 --norm None --early_stop 40 --cv_folds 5 --cont"
 )
 
