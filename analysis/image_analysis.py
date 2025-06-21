2c1b1199a37d1befabf2995701a5ee11d7719550
ketencimert <mk4139@columbia.edu>
2025-06-21 19:04:36 -0400

    image analysis.

--- analysis/image_analysis.py
+++ analysis/image_analysis.py
@@ -13,7 +13,7 @@ from utils import transform
 import numpy as np
 
 import torch
-import random 
+import random
 
 # import sys
 from pathlib import Path
@@ -24,6 +24,7 @@ from utils import prepare_fold, to_np
 
 import matplotlib.pyplot as plt
 import shutil
+
 
 def zip_folder(folder_path, output_path):
     """
@@ -39,6 +40,7 @@ def zip_folder(folder_path, output_path):
     shutil.make_archive(output_path, 'zip', folder_path)
     print(f"Folder '{folder_path}' zipped to '{output_path}.zip'")
 
+
 def find_latest(pattern: str, root: Path):
     """Return the newest file under *root* whose name contains *pattern*."""
     newest_path = None
@@ -48,7 +50,7 @@ def find_latest(pattern: str, root: Path):
         # print(path)
         # if 'fold_X' in path.name.lower():
         if path.is_file() and pattern.lower() in path.name.lower() \
-            and model_name.lower() in path.name.lower() \
+                and model_name.lower() in path.name.lower() \
                 and phi_net.lower() in path.name.lower():
             mtime = path.stat().st_mtime
             if mtime > newest_mtime:
@@ -57,9 +59,20 @@ def find_latest(pattern: str, root: Path):
     print(newest_path)
     return newest_path
 
+
+def find_all(pattern: str, root: Path):
+    """Return the newest file under *root* whose name contains *pattern*."""
+    paths = []
+    for path in root.rglob("*"):
+        if path.is_file() and pattern.lower() in path.name.lower():
+            # print(path)
+            paths.append(str(path))
+    return paths
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    #device args
+    # device args
     parser.add_argument(
         '--device', default='cuda', type=str, help='device to train the model.'
     )
@@ -67,143 +80,141 @@ if __name__ == '__main__':
         '--model_id', default=0, type=int, help='model_id.'
     )
     parser.add_argument(
-        '--dataset', default='mnist_normal_8', type=str
-    )
-    parser.add_argument(
-        '--phi_net', default='masked', type=str
-    )
-    parser.add_argument(
-        '--std', default=1, type=float
+        '--dataset', default='mnist_normal_0', type=str
     )
 
     args = parser.parse_args()
 
-    ###############################################################################
-    ###############################################################################
-    cv_folds = 1
-    k = 1024
-    dataset = args.dataset
-    device = args.device
-    phi_net = args.phi_net
-    model_name =  f'ProbabilisticShapley{str(args.model_id)}'
-    # original_size = 220 #this will be 220 was 224 before and 8 patches
-    ###############################################################################
-    ###############################################################################
-    os.makedirs('./mnist_analysis', exist_ok=True)
-    SEED = 11
-    random.seed(SEED), np.random.seed(SEED), torch.manual_seed(SEED)
-    path = str(find_latest(dataset, Path('./model_checkpoints')))
-
-    # model = torch.load(path).to(device).eval()
-    if 'False' in path.split('preprocess')[-1].split(')')[0][1:]:
-        preprocess = False
-    else:
-        preprocess = True
-    print(f'preprocess {preprocess}')
-    data, features, dtypes, target_scale, predictive_distribution\
-            = load_dataset(dataset)
-    x, y = data[0]
-    d_in = x.shape[-1]
-    n = len(x)
-    tr_size = int(n * 0.7)
-    path = path.split('model_checkpoints/')[-1].replace('.pth','')
-    path = path.split('model_checkpoints\\')[-1].replace('.pth','')
-    folds = np.array(list(range(cv_folds)) * n)[:n]
-    np.random.shuffle(folds)
-    tr_dataloader, val_dataloader, te_dataloader, stats = prepare_fold(
-        x, y, 10, 1, folds,
-        device, torch.float32, dtypes, preprocess
-        )
-    original_size = data[1][2].shape[-1]
-    x = torch.tensor(transform(data[1][0], stats))[:k].float()
-    y = data[1][1][:k]
-    images = data[1][2][:k]
-    
-    prior_loc, prior_scale, predictive_loc, \
-        predictive_scale, predictions \
-        = model.compute_parameters(
-            x.to(device), 
-            model.missingness_indicator(x).to(device)
+    paths = find_all(args.dataset, Path('./model_checkpoints'))
+    for path in paths:
+        try:
+            print(path)
+            ###############################################################################
+            ###############################################################################
+            cv_folds = 1
+            k = 1024
+            dataset = args.dataset
+            device = args.device
+            model_name = f'ProbabilisticShapley{str(args.model_id)}'
+            # original_size = 220 #this will be 220 was 224 before and 8 patches
+            ###############################################################################
+            ###############################################################################
+            os.makedirs('./mnist_analysis', exist_ok=True)
+            SEED = 11
+            random.seed(SEED), np.random.seed(SEED), torch.manual_seed(SEED)
+            model = torch.load(path).to(device).eval()
+            if 'False' in path.split('preprocess')[-1].split(')')[0][1:]:
+                preprocess = False
+            else:
+                preprocess = True
+            print(f'preprocess {preprocess}')
+            data, features, dtypes, target_scale, predictive_distribution \
+                = load_dataset(dataset)
+            x, y = data[0]
+            d_in = x.shape[-1]
+            n = len(x)
+            tr_size = int(n * 0.7)
+            path = path.split('model_checkpoints/')[-1].replace('.pth', '')
+            path = path.split('model_checkpoints\\')[-1].replace('.pth', '')
+            folds = np.array(list(range(cv_folds)) * n)[:n]
+            np.random.shuffle(folds)
+            tr_dataloader, val_dataloader, te_dataloader, stats = prepare_fold(
+                x, y, 10, 1, folds,
+                device, torch.float32, dtypes, preprocess
             )
-    
-    def add_colorbar(mappable):
-        from mpl_toolkits.axes_grid1 import make_axes_locatable
-        import matplotlib.pyplot as plt
-        last_axes = plt.gca()
-        ax = mappable.axes
-        fig = ax.figure
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes("right", size="5%", pad=0.05)
-        cbar = fig.colorbar(mappable, cax=cax)
-        plt.sca(last_axes)
-        return cbar
-    
-    
-    choice_list = []
-    for index, l in enumerate(y):
-        if l != 1:
-            print(index)
-            choice_list.append(index)
-    sd = [0, 1, 2, 3]
-    choice = np.random.choice(choice_list, 5, replace=False).tolist()
-    fig, axes = plt.subplots(nrows=len(sd), ncols=5, figsize=(20,20))
-    # for j, i in enumerate((-predictions).topk(5)[-1]):
-    for k in range(len(sd)):
-        ax = axes[k]
-        for j, i in enumerate(choice):
-            new_size = (original_size, original_size)
-            loc =  prior_loc - 3 * prior_scale
-            resized_prior_loc = cv2.resize(
-                to_np(loc[i]).reshape(
-                    int(loc[i].shape[-1]**0.5),int(loc[i].shape[-1]**0.5),1
-                ), new_size, interpolation=cv2.INTER_CUBIC
-                )
+            original_size = data[1][2].shape[-1]
+            x = torch.tensor(transform(data[1][0], stats))[:k].float()
+            y = data[1][1][:k]
+            images = data[1][2][:k]
 
-            ax[j].imshow(images[i])  # 0.0 (transparent) to 1.0 (opaque)
-            im = ax[j].imshow(resized_prior_loc, cmap='jet', alpha=0.7)
+            prior_loc, prior_scale, predictive_loc, \
+            predictive_scale, predictions \
+                = model.compute_parameters(
+                x.to(device),
+                model.missingness_indicator(x).to(device)
+            )
 
-            add_colorbar(im)
-            ax[j].set_title(f'Prediction: {round(predictions[i].item(), 3)}')
 
-    plt.tight_layout()
-    plt.savefig(f'./mnist_analysis/negative_mnist_{path}.pdf')
-    # plt.show()
-    
-    choice_list = []
-    for index, l in enumerate(y):
-        if l == 1:
-            print(index)
-            choice_list.append(index)
-    sd = [0, 1, 2, 3]
-    choice = np.random.choice(choice_list, 5, replace=False).tolist()
-    fig, axes = plt.subplots(nrows=len(sd), ncols=5, figsize=(20,20))
-    #for j, i in enumerate((predictions).topk(5)[-1]):
-    for k in range(len(sd)):
-        ax = axes[k]
-        for j, i in enumerate(choice):
-            new_size = (original_size, original_size)
-            loc =  prior_loc + sd[k] * prior_scale
-            resized_prior_loc = cv2.resize(
-                to_np(loc[i]).reshape(int(loc[i].shape[-1]**0.5),int(loc[i].shape[-1]**0.5),1), new_size, interpolation=cv2.INTER_CUBIC
-                )
+            def add_colorbar(mappable):
+                from mpl_toolkits.axes_grid1 import make_axes_locatable
+                import matplotlib.pyplot as plt
+                last_axes = plt.gca()
+                ax = mappable.axes
+                fig = ax.figure
+                divider = make_axes_locatable(ax)
+                cax = divider.append_axes("right", size="5%", pad=0.05)
+                cbar = fig.colorbar(mappable, cax=cax)
+                plt.sca(last_axes)
+                return cbar
 
-            ax[j].imshow(images[i])  # 0.0 (transparent) to 1.0 (opaque)
-            im = ax[j].imshow(resized_prior_loc, cmap='jet', alpha=0.7)
-            # cax = inset_axes(ax[j], width="3%", height="100%", loc="lower left",
-            #              bbox_to_anchor=(1.02, 0., 1, 1), bbox_transform=ax[j].transAxes,
-            #              borderpad=1)
-            add_colorbar(im)
-            ax[j].set_title(f'Prediction: {round(predictions[i].item(), 3)}')
-    
-        # divider = make_axes_locatable(ax[j])
-        # cax = divider.append_axes('right', size='5%', pad=0.05)
-        # plt.colorbar(im, ax=ax[j])
-    plt.tight_layout()
-    plt.savefig(f'./mnist_analysis/positive_mnist_{path}.pdf')
 
-    zip_folder('mnist_analysis', 'mnist_analysis')
+            choice_list = []
+            for index, l in enumerate(y):
+                if l != 1:
+                    print(index)
+                    choice_list.append(index)
+            sd = [0, 1, 2, 3]
+            choice = np.random.choice(choice_list, 5, replace=False).tolist()
+            fig, axes = plt.subplots(nrows=len(sd), ncols=5, figsize=(20, 20))
+            # for j, i in enumerate((-predictions).topk(5)[-1]):
+            for k in range(len(sd)):
+                ax = axes[k]
+                for j, i in enumerate(choice):
+                    new_size = (original_size, original_size)
+                    loc = prior_loc - 3 * prior_scale
+                    resized_prior_loc = cv2.resize(
+                        to_np(loc[i]).reshape(
+                            int(loc[i].shape[-1] ** 0.5), int(loc[i].shape[-1] ** 0.5), 1
+                        ), new_size, interpolation=cv2.INTER_CUBIC
+                    )
 
-# from transform import * 
+                    ax[j].imshow(images[i])  # 0.0 (transparent) to 1.0 (opaque)
+                    im = ax[j].imshow(resized_prior_loc, cmap='jet', alpha=0.7)
+
+                    add_colorbar(im)
+                    ax[j].set_title(f'Prediction: {round(predictions[i].item(), 3)}')
+
+            plt.tight_layout()
+            plt.savefig(f'./mnist_analysis/negative_mnist_{path}.pdf')
+            # plt.show()
+
+            choice_list = []
+            for index, l in enumerate(y):
+                if l == 1:
+                    print(index)
+                    choice_list.append(index)
+            sd = [0, 1, 2, 3]
+            choice = np.random.choice(choice_list, 5, replace=False).tolist()
+            fig, axes = plt.subplots(nrows=len(sd), ncols=5, figsize=(20, 20))
+            # for j, i in enumerate((predictions).topk(5)[-1]):
+            for k in range(len(sd)):
+                ax = axes[k]
+                for j, i in enumerate(choice):
+                    new_size = (original_size, original_size)
+                    loc = prior_loc + sd[k] * prior_scale
+                    resized_prior_loc = cv2.resize(
+                        to_np(loc[i]).reshape(int(loc[i].shape[-1] ** 0.5), int(loc[i].shape[-1] ** 0.5), 1), new_size,
+                        interpolation=cv2.INTER_CUBIC
+                    )
+
+                    ax[j].imshow(images[i])  # 0.0 (transparent) to 1.0 (opaque)
+                    im = ax[j].imshow(resized_prior_loc, cmap='jet', alpha=0.7)
+                    # cax = inset_axes(ax[j], width="3%", height="100%", loc="lower left",
+                    #              bbox_to_anchor=(1.02, 0., 1, 1), bbox_transform=ax[j].transAxes,
+                    #              borderpad=1)
+                    add_colorbar(im)
+                    ax[j].set_title(f'Prediction: {round(predictions[i].item(), 3)}')
+
+                # divider = make_axes_locatable(ax[j])
+                # cax = divider.append_axes('right', size='5%', pad=0.05)
+                # plt.colorbar(im, ax=ax[j])
+            plt.tight_layout()
+            plt.savefig(f'./mnist_analysis/positive_mnist_{path}.pdf')
+
+            zip_folder('mnist_analysis', 'mnist_analysis')
+        except:
+            pass
+# from transform import *
 # # i = 3
 # autoencoder = torch.load('./transform_mnist_8_segments.pt')
 # train_loader = torch.utils.data.DataLoader(
@@ -223,7 +234,7 @@ if __name__ == '__main__':
 #         choice_list.append(index)
 
 # i = np.random.choice(choice_list, 1).item()
-  
+
 # encoded_input = autoencoder.encoder(autoencoder_input[0].cuda())
 # encoded_input_to_shapley_net = torch.tensor(
 #     transform(to_np(encoded_input), stats)
@@ -231,7 +242,7 @@ if __name__ == '__main__':
 # prior_loc, prior_scale, predictive_loc, \
 #     predictive_scale, predictions \
 #     = model.compute_loc_scale(
-#         encoded_input_to_shapley_net, 
+#         encoded_input_to_shapley_net,
 #         torch.ones_like(encoded_input_to_shapley_net)
 #         )
 # plt.imshow(to_np(autoencoder_input[0][i]).reshape(224,224))
@@ -289,5 +300,5 @@ if __name__ == '__main__':
 #     )
 # plt.imshow(images[i])  # 0.0 (transparent) to 1.0 (opaque)
 # plt.imshow(resized_prior_loc, cmap='jet', alpha=0.6)
-# plt.colorbar() 
+# plt.colorbar()
 # plt.show()
